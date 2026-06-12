897c6401c089ca11ca4f2050b26c0f3ba2773046
wht04211228-arch <wht04211228@gmail.com>
2026-06-12 17:46:08 +0800

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-// config.js 示例
-const config = {
-  userName: '小明',
-  music: 'assets/music/birthday.mp3',
-  photos: ['assets/photos/1.jpg','assets/photos/2.jpg'],
-  messages: ["生日快乐！🎂","愿你永远开心💖"]
-};
